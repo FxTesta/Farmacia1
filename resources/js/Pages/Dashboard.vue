7ec94fff46c69d97516643020d3eb16cda337f03
canteroaa <ocampocanteroaaron@gmail.com>
2023-05-01 20:17:33 -0400

    pruebaa

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,8 +1,18 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import { Head,useForm } from '@inertiajs/vue3';
 import SideBar from '@/Components/SideBar.vue';
 import TopBar from '@/Layouts/TopBar.vue';
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import TextInput from '@/Components/TextInput.vue';
+
+const form = useForm({
+    name: '',
+    lala: '',
+
+});
 </script>
 
 <template>
@@ -21,6 +31,55 @@ import TopBar from '@/Layouts/TopBar.vue';
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                    
                     <div class="p-6 text-gray-900">You're logged in!</div>
+                    <!--PRUEBAAAAAAAAAAAAAAAAAAAAAAAA-->
+                    <form @submit.prevent="submit">
+
+                            <div class="ml-2 inline-flex space-x-2">
+
+                                <div>
+                                    <InputLabel for="name" value="Nombre" class="text-gray-600"/>
+
+                                    <TextInput
+                                        id="name"
+                                        type="text"
+                                        class="mt-1 block  bg-gray-200 text-gray-600"
+                                        v-model="form.name"
+                                        required
+                                        autofocus
+                                        autocomplete="name"
+                                    />
+
+                                    <InputError class="mt-2" :message="form.errors.name" />
+                                </div>
+
+                                <div>
+                                    <InputLabel for="lala" value="Apellido" class="text-gray-600"/>
+
+                                    <TextInput
+                                        id="lala"
+                                        type="text"
+                                        class="mt-1 block  bg-gray-200 text-gray-600"
+                                        v-model="form.lala"
+                                        required
+                                        autofocus
+                                        autocomplete="lala"
+                                    />
+
+                                    <InputError class="mt-2" :message="form.errors.lala" />
+                                </div>
+
+                            </div>
+
+                            <div class="flex items-center justify-end mt-6">
+
+                                <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                                    Crear Cliente
+                                </PrimaryButton>
+                            </div>
+                        </form> 
+
+                    <!--PRUEBAAAAAAAAAAAAAAAAAAAAAAAA-->
+
                 </div>
             </div>
         </div>
