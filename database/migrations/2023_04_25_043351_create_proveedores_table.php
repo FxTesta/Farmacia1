b63e3cb1266e0d2949c396c2e6bb61f3ddeb4579
FxTesta <andrestesta111@fpuna.edu.py>
2023-05-28 01:35:48 -0400

    se agrego nombre de empresa y contacto

--- database/migrations/2023_04_25_043351_create_proveedores_table.php
+++ database/migrations/2023_04_25_043351_create_proveedores_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('proveedores', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('name');
             $table->bigInteger('cedula');
             $table->string('ruc')->nullable();
