92c45535eade2f23dcfa6f0614ccc60d6e96aa14
alexxmartt <martinezalexis073@gmail.com>
2023-09-22 20:11:54 -0400

    asd

--- app/Http/Controllers/StockAuditController.php
+++ app/Http/Controllers/StockAuditController.php
@@ -12,13 +12,13 @@ class StockAuditController extends Controller
     {   
        // $productos= StockAudit::all();
       // $productos = StockAudit::with('id', 'usuario', 'id_articulo', 'articulo','stockanterior','stockactual')->get();
-      $fechas = $request->input('valor');
-       
-    //  $productos = DB::table('stock_audits')
-     //  ->whereDate('fecha', '>=', $fecha)
-     //  ->get();
-    //  return view('template',compact('productos'))
-    return view('template',compact('fechas'));
+      $fecha = $request->input('valor');       
+      $productos = DB::table('stock_audits')
+       ->whereDate('fecha', '>=', $fecha)
+       ->get();
+
+      return view('template',compact('productos'));
+   //return view('template',compact('fechas'));
       // return view('template')->with('fecha', $fecha);
        //return view('template', ['fecha' => $fecha]);
        // return $fechas;
